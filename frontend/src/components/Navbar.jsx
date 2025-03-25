c0a2a458b2b5f89eebfdacb17ca372a427ff64d0
Ridhika Madan <ridhikamadan22gmail.com@Ridhikas-MacBook-Air.local>
2025-03-24 22:22:42 -0400

    Updated navbar logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,9 +3,10 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
 	return (
 		<header className="fixed top-0 w-full z-50 px-6 py-5 flex justify-between items-center backdrop-blur-sm bg-transparent">
-			<a href="/" className="text-xl sm:text-2xl font-extrabold tracking-wide">
-				WatCourse
+			<a href="/" className="text-xl sm:text-2xl font-medium tracking-wide">
+ 			 <span style={{ color: "#FED34C" }}>Wat</span>Course
 			</a>
+
 			<nav className="flex gap-6 text-sm sm:text-base font-medium text-white">
 				<a href="#faq" className="hover:text-[#FED34C] transition">
 					FAQ
