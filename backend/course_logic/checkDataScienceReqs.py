3928afe5edbd0c266b14acf96802296b32a0b190
Priyanshu Singhal <150108714+priyanshusinghal12@users.noreply.github.com>
2025-04-07 22:01:39 -0400

    data science and math physics

--- backend/course_logic/checkDataScienceReqs.py
+++ backend/course_logic/checkDataScienceReqs.py
@@ -1,65 +1,65 @@
 from course_logic.helper import *
 
 def check_data_science_major(student_courses):
-
+ 
     refine_courses(student_courses, ["CS 136L", "CS 136", "CS 146", "ENGL 378", "MTHEL 300",
      "MATH 237", "MATH 247", "MATH 239", "MATH 249"])
-
+ 
     data_science_reqs = {
-        "Complete all of the following: CS136L, CS341, CS348, CS431, STAT330, STAT331, STAT332, STAT333, STAT341": [False, []],
-        "Complete 1 of the following: CS136, CS146": [False, []],
-        "Complete 1 of the following: CS240, CS240E": [False, []],
-        "Complete 1 of the following: CS241, CS241E": [False, []],
-        "Complete 1 of the following: CS245, CS245E": [False, []],
-        "Complete 1 of the following: CS246, CS246E": [False, []],
-        "Complete 1 of the following: CS251, CS251E": [False, []],
-        "Complete 1 of the following: CS480, CS484, CS485, CS486, STAT441": [False, []],
-        "Complete 2 of the following: STAT431, STAT440, STAT441, STAT442, STAT443, STAT444": [False, []],
-        "Complete 1 of the following: ENGL378, MTHEL300": [False, []],
-        "Complete 1 of the following: MATH237, MATH247": [False, []],
-        "Complete 1 of the following: MATH239, MATH249": [False, []],
+        "Complete all of the following: CS 136L, CS 341, CS 348, CS 431, STAT 330, STAT 331, STAT 332, STAT 333, STAT 341": [False, []],
+        "Complete 1 of the following: CS 136, CS 146": [False, []],
+        "Complete 1 of the following: CS 240, CS 240E": [False, []],
+        "Complete 1 of the following: CS 241, CS 241E": [False, []],
+        "Complete 1 of the following: CS 245, CS 245E": [False, []],
+        "Complete 1 of the following: CS 246, CS 246E": [False, []],
+        "Complete 1 of the following: CS 251, CS 251E": [False, []],
+        "Complete 1 of the following: CS 480, CS 484, CS 485, CS 486, STAT 441": [False, []],
+        "Complete 2 of the following: STAT 431, STAT 440, STAT 441, STAT 442, STAT 443, STAT 444": [False, []],
+        "Complete 1 of the following: ENGL 378, MTHEL 300": [False, []],
+        "Complete 1 of the following: MATH 237, MATH 247": [False, []],
+        "Complete 1 of the following: MATH 239, MATH 249": [False, []],
         "Complete 1 additional course at the 300- or 400-level from: ACTSC, AMATH, CO, CS, MATBUS, MATH, PMATH, STAT": [False, []]
     }
-
+ 
     # Req 1: Core courses
-    check_complete_all("Complete all of the following: CS136L, CS341, CS348, CS431, STAT330, STAT331, STAT332, STAT333, STAT341",
-                        ["CS136L", "CS341", "CS348", "CS431", "STAT330", "STAT331", "STAT332", "STAT333", "STAT341"],
+    check_complete_all("Complete all of the following: CS 136L, CS 341, CS 348, CS 431, STAT 330, STAT 331, STAT 332, STAT 333, STAT 341",
+                        ["CS 136L", "CS 341", "CS 348", "CS 431", "STAT 330", "STAT 331", "STAT 332", "STAT 333", "STAT 341"],
                         student_courses, data_science_reqs)
-
+ 
     # Req 2-7: One course from each list
-    check_n_from_list("Complete 1 of the following: CS136, CS146",
-                      ["CS136", "CS146"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
-    check_n_from_list("Complete 1 of the following: CS240, CS240E",
-                      ["CS240", "CS240E"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
-    check_n_from_list("Complete 1 of the following: CS241, CS241E",
-                      ["CS241", "CS241E"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
-    check_n_from_list("Complete 1 of the following: CS245, CS245E",
-                      ["CS245", "CS245E"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
-    check_n_from_list("Complete 1 of the following: CS246, CS246E",
-                      ["CS246", "CS246E"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
-    check_n_from_list("Complete 1 of the following: CS251, CS251E",
-                      ["CS251", "CS251E"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
-
+    check_n_from_list("Complete 1 of the following: CS 136, CS 146",
+                      ["CS 136", "CS 146"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
+    check_n_from_list("Complete 1 of the following: CS 240, CS 240E",
+                      ["CS 240", "CS 240E"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
+    check_n_from_list("Complete 1 of the following: CS 241, CS 241E",
+                      ["CS 241", "CS 241E"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
+    check_n_from_list("Complete 1 of the following: CS 245, CS 245E",
+                      ["CS 245", "CS 245E"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
+    check_n_from_list("Complete 1 of the following: CS 246, CS 246E",
+                      ["CS 246", "CS 246E"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
+    check_n_from_list("Complete 1 of the following: CS 251, CS 251E",
+                      ["CS 251", "CS 251E"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
+ 
     # Req 8: One course from machine learning list
-    check_n_from_list("Complete 1 of the following: CS480, CS484, CS485, CS486, STAT441",
-                      ["CS480", "CS484", "CS485", "CS486", "STAT441"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
-
+    check_n_from_list("Complete 1 of the following: CS 480, CS 484, CS 485, CS 486, STAT 441",
+                      ["CS 480", "CS 484", "CS 485", "CS 486", "STAT 441"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
+ 
     # Req 9: Two courses from stats list
-    check_n_from_list("Complete 2 of the following: STAT431, STAT440, STAT441, STAT442, STAT443, STAT444",
-                      ["STAT431", "STAT440", "STAT441", "STAT442", "STAT443", "STAT444"], n=2, student_courses=student_courses, major_reqs=data_science_reqs)
-
+    check_n_from_list("Complete 2 of the following: STAT 431, STAT 440, STAT 441, STAT 442, STAT 443, STAT 444",
+                      ["STAT 431", "STAT 440", "STAT 441", "STAT 442", "STAT 443", "STAT 444"], n=2, student_courses=student_courses, major_reqs=data_science_reqs)
+ 
     #Req 10: Communication Course
-    check_n_from_list("Complete 1 of the following: ENGL378, MTHEL300",
-                      ["ENGL378", "MTHEL300"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
-
+    check_n_from_list("Complete 1 of the following: ENGL 378, MTHEL 300",
+                      ["ENGL 378", "MTHEL 300"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
+ 
     #Req 11:  Calculus 2
-    check_n_from_list("Complete 1 of the following: MATH237, MATH247",
-                      ["MATH237", "MATH247"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
-
+    check_n_from_list("Complete 1 of the following: MATH 237, MATH 247",
+                      ["MATH 237", "MATH 247"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
+ 
     #Req 12: C&O course
-    check_n_from_list("Complete 1 of the following: MATH239, MATH249",
-                      ["MATH239", "MATH249"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
-
+    check_n_from_list("Complete 1 of the following: MATH 239, MATH 249",
+                      ["MATH 239", "MATH 249"], n=1, student_courses=student_courses, major_reqs=data_science_reqs)
+ 
     #Req 13: 1 additional course at 300/400 level from ACTSC, AMATH, CO, CS, MATBUS, MATH, PMATH, STAT
     check_n_courses("Complete 1 additional course at the 300- or 400-level from: ACTSC, AMATH, CO, CS, MATBUS, MATH, PMATH, STAT",
                     eligible_levels=300,
@@ -67,6 +67,5 @@ def check_data_science_major(student_courses):
                     n=1,
                     student_courses=student_courses,
                     major_reqs=data_science_reqs)
-
+ 
     return data_science_reqs
-
--- backend/course_logic/checkMathPhysicsReqs.py
+++ backend/course_logic/checkMathPhysicsReqs.py
@@ -1,75 +1,75 @@
 from course_logic.helper import *
-
+ 
 def check_math_physics_reqs(student_courses):
-
+ 
     refine_courses(student_courses, ["MATH 237", "MATH 247"])
-
+ 
     math_physics_reqs = {
-        "Complete all the following: AMATH 231, AMATH 271, AMATH 351, AMATH 353, AMATH 361, AMATH 373, PHYS 121, PHYS 122, PHYS 234, PHYS 242": [False, []],
-        "Complete one of the following: AMATH 242, CS 371": [False, []],
+        "Complete all the following: AMATH 231, AMATH 271, AMATH 353, AMATH 361, PHYS 121, PHYS 122, PHYS 242": [False, []],
+        "Complete one of the following: AMATH 242, AMATH 345, AMATH 391, AMATH 445, CS 371": [False, []],
         "Complete one of the following: AMATH 250, AMATH 251": [False, []],
         "Complete one of the following: AMATH 331, PMATH 331, PMATH 333, PMATH 351": [False, []],
         "Complete one of the following: AMATH 332, PMATH 332, PMATH 352": [False, []],
+        "Complete either AMATH 373, or both of PHYS 234 and PHYS 334": [False, []],
         "Complete one of the following: AMATH 473, PHYS 454": [False, []],
         "Complete one of the following: AMATH 475, PHYS 476": [False, []],
+        "Complete 2 of the following: AMATH 333, AMATH 474, PHYS 359, PHYS 484": [False, []],
+        "Complete one of the following: ECE 403, PHYS 358": [False, []],
         "Complete one of the following: MATH 237, MATH 247": [False, []],
-        "Complete two of the following: ECE 403, PHYS 342, PHYS 358, PHYS 359, PHYS 363": [False, []],
-        "Complete 2 additional courses at 300/400 level from AMATH, PHYS": [False, []],
-        "Complete 5 additional courses from AMATH or PHYS": [False, []]
+        "Complete 1.5 additional units from AMATH or PHYS at the 300 or 400 level": [False, []],
+        "Complete 1.5 additional units from AMATH or PHYS at any level": [False, []]
     }
-
+ 
     # Req 1: Core AMATH and PHYS courses
-    check_complete_all("Complete all the following: AMATH 231, AMATH 271, AMATH 351, AMATH 353, AMATH 361, AMATH 373, PHYS 121, PHYS 122, PHYS 234, PHYS 242",
-                       ["AMATH 231", "AMATH 271", "AMATH 351", "AMATH 353", "AMATH 361", "AMATH 373",
-                        "PHYS 121", "PHYS 122", "PHYS 234", "PHYS 242"],
+    check_complete_all("Complete all the following: AMATH 231, AMATH 271, AMATH 353, AMATH 361, PHYS 121, PHYS 122, PHYS 242",
+                       ["AMATH 231", "AMATH 271", "AMATH 353", "AMATH 361", "PHYS 121", "PHYS 122", "PHYS 242"],
                        student_courses, math_physics_reqs)
-
-    # Req 2: Computational Mathematics course
-    check_n_from_list("Complete one of the following: AMATH 242, CS 371",
-                      ["AMATH 242", "CS 371"], n=1, student_courses=student_courses, major_reqs=math_physics_reqs)
-
+ 
+    # Req 2
+    check_n_from_list("Complete one of the following: AMATH 242, AMATH 345, AMATH 391, AMATH 445, CS 371",
+                      ["AMATH 242", "AMATH 345", "AMATH 391", "AMATH 445", "CS 371"], n=1, student_courses=student_courses, major_reqs=math_physics_reqs)
+ 
     # Req 3: Differential Equations course
     check_n_from_list("Complete one of the following: AMATH 250, AMATH 251",
                       ["AMATH 250", "AMATH 251"], n=1, student_courses=student_courses, major_reqs=math_physics_reqs)
-
+ 
     # Req 4: Real Analysis course
     check_n_from_list("Complete one of the following: AMATH 331, PMATH 331, PMATH 333, PMATH 351",
                       ["AMATH 331", "PMATH 331", "PMATH 333", "PMATH 351"], n=1, student_courses=student_courses, major_reqs=math_physics_reqs)
-
+ 
     # Req 5: Complex Analysis course
     check_n_from_list("Complete one of the following: AMATH 332, PMATH 332, PMATH 352",
                       ["AMATH 332", "PMATH 332", "PMATH 352"], n=1, student_courses=student_courses, major_reqs=math_physics_reqs)
-
+ 
     # Req 6: Quantum Theory 2 course
+    check_n_from_list("Complete either AMATH 373, or both of PHYS 234 and PHYS 334",
+                      ["AMATH 373"], n=1, student_courses=student_courses, major_reqs=math_physics_reqs)
+ 
+    if not math_physics_reqs["Complete either AMATH 373, or both of PHYS 234 and PHYS 334"][0]:
+        check_n_from_list("Complete either AMATH 373, or both of PHYS 234 and PHYS 334",
+                      ["PHYS 234", "PHYS 334"], n=2, student_courses=student_courses, major_reqs=math_physics_reqs)
+ 
     check_n_from_list("Complete one of the following: AMATH 473, PHYS 454",
-                      ["AMATH 473", "PHYS 454"], n=1, student_courses=student_courses, major_reqs=math_physics_reqs)
-
-    # Req 7: General Relativity course
+                      ["AMATH 473", "PHYS 454"], 1, student_courses, math_physics_reqs)
+ 
+    # Req: General Relativity course
     check_n_from_list("Complete one of the following: AMATH 475, PHYS 476",
                       ["AMATH 475", "PHYS 476"], n=1, student_courses=student_courses, major_reqs=math_physics_reqs)
-
-    # Req 8: Advanced Calculus course
+ 
+    check_n_from_list("Complete 2 of the following: AMATH 333, AMATH 474, PHYS 359, PHYS 484",
+                      ["AMATH 333", "AMATH 474", "PHYS 359", "PHYS 484"], n=2, student_courses=student_courses, major_reqs=math_physics_reqs)
+ 
+    check_n_from_list("Complete one of the following: ECE 403, PHYS 358",
+                      ["ECE 403", "PHYS 358"], n=1, student_courses=student_courses, major_reqs=math_physics_reqs)
+ 
+    # Req 9: Advanced Calculus course
     check_n_from_list("Complete one of the following: MATH 237, MATH 247",
                       ["MATH 237", "MATH 247"], n=1, student_courses=student_courses, major_reqs=math_physics_reqs)
-
-    # Req 9: Two advanced physics/electrical engineering courses
-    check_n_from_list("Complete two of the following: ECE 403, PHYS 342, PHYS 358, PHYS 359, PHYS 363",
-                      ["PHYS 342", "PHYS 358", "PHYS 359", "PHYS 363", "ECE 403"], n=2, student_courses=student_courses, major_reqs=math_physics_reqs)
-
-    # Req 10: Two additional 300/400 level AMATH or PHYS courses
-    check_n_courses("Complete 2 additional courses at 300/400 level from AMATH, PHYS",
-                    eligible_levels=300,
-                    subject_codes=["AMATH", "PHYS"],
-                    n=2,
-                    student_courses=student_courses,
-                    major_reqs=math_physics_reqs)
-
-    # Req 11: Five additional AMATH or PHYS courses
-    check_n_courses("Complete 5 additional courses from AMATH or PHYS",
-                    eligible_levels=100,
-                    subject_codes=["AMATH", "PHYS"],
-                    n=5,
-                    student_courses=student_courses,
-                    major_reqs=math_physics_reqs)
-
+ 
+    check_n_courses("Complete 1.5 additional units from AMATH or PHYS at the 300 or 400 level",
+                    300, ["AMATH", "PHYS"], 3, student_courses, math_physics_reqs)
+ 
+    check_n_courses("Complete 1.5 additional units from AMATH or PHYS at any level",
+                    100, ["AMATH", "PHYS"], 3, student_courses, math_physics_reqs)
+ 
     return math_physics_reqs
