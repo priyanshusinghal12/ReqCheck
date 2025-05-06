eb2d34ae1f0074943670104a2cb0728b14267a7c
Priyanshu Singhal <150108714+priyanshusinghal12@users.noreply.github.com>
2025-05-06 10:34:40 +0530

    math studies

--- backend/course_logic/checkMathStudiesReqs.py
+++ backend/course_logic/checkMathStudiesReqs.py
@@ -1,17 +1,17 @@
 from course_logic.helper import *
-
-
+ 
+ 
 def math_studies_reqs(student_courses):
     """
     Checks if a student has completed the requirements for the General Math Major.
-
+ 
     Args:
         student_courses: A list of student's completed courses.
-
+ 
     Returns:
         A dictionary of the program requirements and their completion status.
     """
-
+ 
     math_studies_reqs = {
         "Complete one of the following: MATH 106, MATH 136, MATH 146": [False, []],
         "Complete one of the following: MATH 127, MATH 137, MATH 147": [False, []],
@@ -31,9 +31,9 @@ def math_studies_reqs(student_courses):
         "Two additional math courses or two courses that contribute to a minor outside of Mathematics": [False, []],
         "Four free-choice electives": [False, []]
     }
-
+ 
     math_subject_codes = ["ACTSC", "AMATH", "CO", "CS", "MATBUS", "MATH", "PMATH", "STAT"]
-
+ 
     # Requirements 1-10: Specific course requirements
     check_n_from_list("Complete one of the following: MATH 106, MATH 136, MATH 146",
                       ["MATH 106", "MATH 136", "MATH 146"], n=1, student_courses=student_courses, major_reqs=math_studies_reqs)
@@ -55,24 +55,24 @@ def math_studies_reqs(student_courses):
                       ["CS 115", "CS 135", "CS 145"], n=1, student_courses=student_courses, major_reqs=math_studies_reqs)
     check_n_from_list("Complete one of the following: CS 116, CS 136, CS 146",
                       ["CS 116", "CS 136", "CS 146"], n=1, student_courses=student_courses, major_reqs=math_studies_reqs)
-
+ 
     # Requirement 11: Two EMLS/ENGL/SPCOM courses satisfying the Communication Skills requirement
-
+ 
     # Lists of courses for the communication skills requirement
     list1_courses = ["SPCOM 100", "SPCOM 223", "EMLS 101R", "EMLS 102R", "EMLS 129R", "ENGL 109", "ENGL 129R"]
-
+ 
     list2_courses = ["SPCOM 225", "SPCOM 227", "SPCOM 228", "EMLS 103R", "EMLS 104R", "EMLS 110R",
                      "ENGL 101B", "ENGL 108B", "ENGL 108D", "ENGL 119", "ENGL 208B", "ENGL 209", "ENGL 210E",
                      "ENGL 210F", "ENGL 378", "MTHEL 300"]
-
+ 
     # Check for the first communication skills course (from List 1)
     check_n_from_list("1st Communication Skills Requirement",
                       list1_courses, n=1, student_courses=student_courses, major_reqs=math_studies_reqs)
-
+ 
     check_n_from_list("2nd Communication Skills Requirement",
                       list1_courses + list2_courses, n=1, student_courses=student_courses, major_reqs=math_studies_reqs)
-
-
+ 
+ 
     # Requirement 12: Ten mathematics 3XX and/or 4XX courses
     check_n_courses("Ten mathematics 3XX and/or 4XX courses",
                     eligible_levels=300,
@@ -80,7 +80,7 @@ def math_studies_reqs(student_courses):
                     n=10,
                     student_courses=student_courses,
                     major_reqs=math_studies_reqs)
-
+ 
     # Requirement 13: Four additional mathematics courses
     check_n_courses("Four additional mathematics courses",
                     eligible_levels=100,
@@ -88,7 +88,7 @@ def math_studies_reqs(student_courses):
                     n=4,
                     student_courses=student_courses,
                     major_reqs=math_studies_reqs)
-
+ 
     # Requirement 14: Eight additional non-math courses
     non_math_courses = [course for course in student_courses if course.split(" ")[0] not in math_subject_codes]
     if len(non_math_courses) >= 8:
@@ -96,7 +96,11 @@ def math_studies_reqs(student_courses):
         math_studies_reqs["Eight additional non-math courses"][0] = True
         math_studies_reqs["Eight additional non-math courses"][1].extend(non_math_courses)
         student_courses = [course for course in student_courses if course not in non_math_courses]
-
+    
+    if not math_studies_reqs["Eight additional non-math courses"][0]:
+        math_studies_reqs["Eight additional non-math courses"][1].extend(non_math_courses)
+        student_courses = [course for course in student_courses if course not in non_math_courses]
+ 
     # Requirements 15: 2 additional math courses.
     check_n_courses("Two additional math courses or two courses that contribute to a minor outside of Mathematics",
                     eligible_levels=100,
@@ -106,7 +110,7 @@ def math_studies_reqs(student_courses):
                     major_reqs=math_studies_reqs)
     
     # print("student courses:", student_courses)
-
+ 
     #Req 16 - 4 free choice electives.
     if len(student_courses) >= 4:
         student_courses = student_courses[:4]
@@ -117,7 +121,7 @@ def math_studies_reqs(student_courses):
         math_studies_reqs["Four free-choice electives"][0] = False
         math_studies_reqs["Four free-choice electives"][1].extend(student_courses)
         student_courses = []
-
-
+ 
+ 
     return math_studies_reqs
-
+ 
